6727bfa1193cc0c0e2a32e46341c3de911383e48
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 17:24:38 +0530

    update : contactInfo.js => made way for props

--- pangea_ui/src/Components/Contact/ContactInfo.js
+++ pangea_ui/src/Components/Contact/ContactInfo.js
@@ -1,23 +1,23 @@
 import React from "react";
 
-const ContactInfo = () => {
+const ContactInfo = ({ address, contact, email }) => {
 	return (
 		<div className="contact">
 			<h3>Hyderabad</h3>
 
 			<div className="address">
 				<p>Address</p>
-				<p>Hello Hello HEllo</p>
+				<p>{address}</p>
 			</div>
 
 			<div className="mobile">
 				<p>Contact</p>
-				<p>+123456789</p>
+				<p>{contact}</p>
 			</div>
 
 			<div className="mail">
 				<p>Write to us</p>
-				<p>hello @ hello.com</p>
+				<p>{email}</p>
 			</div>
 		</div>
 	);
