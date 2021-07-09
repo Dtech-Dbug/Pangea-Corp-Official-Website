8271b18830cd377310e3942f14e753e842700303
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 12:40:36 +0530

    docs : components/services/webApp.js

---
+++ pangea_ui/src/Components/Services/WebApp.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WebApp = () => {
+	return (
+		<div class="container">
+			<h2>Web & app development services.</h2>
+
+			<p>
+				At Pangaea Corp, we partner with clients to deliver transformative
+				digital experiences changing the way product leaders think about app
+				design and software development, we help companies of all scale bring
+				new software products to market with speed and expertise.
+			</p>
+		</div>
+	);
+};
+
+export default WebApp;
