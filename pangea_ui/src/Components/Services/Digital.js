26b48ffbe0584db2ecbc5ecb6ba7e2395f76ec32
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 12:41:09 +0530

    docs : services/Digital.js

---
+++ pangea_ui/src/Components/Services/Digital.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Digital = () => {
+	return (
+		<div>
+			<h2>Digital Transformation</h2>
+
+			<p>
+				Radically transform the way you apply technology to serve your customers
+				through our digital transformation program, a consultative partnership
+				that uses a structured approach to help organizations define a new
+				vision.
+			</p>
+
+			<ul>
+				<li> Product design & development.</li>
+				<li> Alignment workshop.</li>
+				<li>Research planning.</li>
+				<li> Business & market analysis.</li>
+				<li>Customer experience assessment.</li>
+				<li> Technical research.</li>
+				<li> Future recommendations.</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Digital;
