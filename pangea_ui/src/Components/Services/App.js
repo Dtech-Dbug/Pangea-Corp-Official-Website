cf7c35c26409488e99639075de117b2e5482943b
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 12:41:19 +0530

    docs : services/App.js

---
+++ pangea_ui/src/Components/Services/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const AppDev = () => {
+	return (
+		<div>
+			<h2>App Development.</h2>
+
+			<p>
+				Bring your iOS or Android app to market quickly with a team of
+				experienced mobile app developers and designers. Our software developers
+				and designers provide deep expertise in the top development frameworks,
+				including React Native.{" "}
+			</p>
+
+			<ul>
+				<li>Fully responsive design and development.</li>
+				<li>React Native mobile app development.</li>
+				<li>Modern frameworks – React.js, Ruby on Rails, Laravel PHP.</li>
+				<li>Python, Vue.js.</li>
+				<li>Full stack development.</li>
+				<li>Google Cloud + AWS infrastructure planning + Management.</li>
+				<li>AI & machine learning.</li>
+				<li>Internet of Things (IoT). </li>
+			</ul>
+		</div>
+	);
+};
+
+export default AppDev;
