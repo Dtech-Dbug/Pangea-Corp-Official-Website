ddc55c6ed50f98b502f34a6b634ff46bde737823
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 15:44:40 +0530

    Update & fix : Services/Service.js => fixed typo

--- pangea_ui/src/Components/Services/Service.js
+++ pangea_ui/src/Components/Services/Service.js
@@ -2,7 +2,6 @@ import React from "react";
 import WebApp from "./WebApp";
 import UI_UX from "./UI_UX";
 import AppDev from "./AppDev";
-import Digial from "./Digital";
 import Digital from "./Digital";
 
 const Service = () => {
