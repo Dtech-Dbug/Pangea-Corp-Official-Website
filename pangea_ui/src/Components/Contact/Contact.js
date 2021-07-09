57f470ca8edf8ab8bdcb4f42ce95c27935204294
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 21:20:48 +0530

    update : contact.js

--- pangea_ui/src/Components/Contact/Contact.js
+++ pangea_ui/src/Components/Contact/Contact.js
@@ -6,9 +6,30 @@ import ContactInfo from "./ContactInfo";
 
 const Contact = () => {
 	return (
-		<div className="contact-container">
+		<div className="contact-container" id="contect">
 			<h2>Contact</h2>
-			<ContactInfo address="hello" contact="+122444" email="hello@hello" />
+
+			<ul>
+				<li>Hyderabad</li>
+				<li>Hyderabad</li>
+				<li>Hyderabad</li>
+			</ul>
+
+			<ContactInfo
+				address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
+				contact="+0124 415 8000"
+				email="hello@parngeacrop.com"
+			/>
+			<ContactInfo
+				address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
+				contact="+0124 415 8000"
+				email="hello@parngeacrop.com"
+			/>
+			<ContactInfo
+				address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
+				contact="+0124 415 8000"
+				email="hello@parngeacrop.com"
+			/>
 		</div>
 	);
 };
