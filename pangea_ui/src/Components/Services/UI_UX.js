4cae21428977a92c866813be5b926fafe6c4b046
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 12:40:48 +0530

    docs : services/Ui_UX.js

---
+++ pangea_ui/src/Components/Services/UI_UX.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const UI_UX = () => {
+	return (
+		<div>
+			<h2>UI/UX Design</h2>
+
+			<p>
+				Design, build, and transform your user’s journey using a highly
+				experienced and disciplined team of researchers and product designers.
+				We offer strategy and service design workshops, UX research, and UI/UX
+				design and prototyping capabilities.
+			</p>
+
+			<ul>
+				<li>User experience design</li>
+				<li>User interface design</li>
+				<li>Interaction design</li>
+				<li>Service design</li>
+				<li>Product design</li>
+				<li>Experience prototyping</li>
+				<li>Design thinking workshop facilitation </li>
+			</ul>
+		</div>
+	);
+};
+
+export default UI_UX;
