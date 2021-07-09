a3cc992a944bccbe8e235cb3e1e570abf444e140
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 12:41:52 +0530

    create : services/service.js
    - imported all other files in the service folder inside this service.js file

---
+++ pangea_ui/src/Components/Services/Service.js
@@ -0,0 +1,17 @@
+import React from "react";
+import WebApp from "./WebApp";
+import UI_UX from "./UI_UX";
+import AppDev from "./App";
+import Digial from "./Digital";
+
+const Service = () => {
+	return (
+		<div class="container" id="services">
+			<WebApp />
+			<UI_UX />
+			<AppDev />
+		</div>
+	);
+};
+
+export default Service;
