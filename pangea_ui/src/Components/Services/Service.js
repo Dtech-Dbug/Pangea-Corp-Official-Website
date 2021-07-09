6791a375f6020de85b117d1386a86198cac0cf59
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 16:49:43 +0530

    Docs : created components/contact
    - created empty files under component folder

--- pangea_ui/src/Components/Services/Service.js
+++ pangea_ui/src/Components/Services/Service.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import WebApp from "./WebApp";
 import UI_UX from "./UI_UX";
 import AppDev from "./AppDev";
 import Digital from "./Digital";
 
+//animation
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 const Service = () => {
+	useEffect(() => {
+		AOS.init({
+			offset: 700,
+			duration: 900,
+		});
+	}, []);
 	return (
 		<div id="services">
 			<WebApp />
