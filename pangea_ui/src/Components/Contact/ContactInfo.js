5c61bd9e25697c480c96d4c1d879a33f5890bbf1
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 16:54:42 +0530

    docs : create Dummy component contactInfo.js

---
+++ pangea_ui/src/Components/Contact/ContactInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ContactInfo = () => {
+	return (
+		<>
+			<h2>Hyderabad</h2>
+
+			<h4>Address</h4>
+			<p>Hello , check check check check</p>
+
+			<h4>Contact No.</h4>
+			<p>123456789</p>
+
+			<h4>Write To Us</h4>
+			<p>Hello @ hello.com</p>
+		</>
+	);
+};
+
+export default ContactInfo;
