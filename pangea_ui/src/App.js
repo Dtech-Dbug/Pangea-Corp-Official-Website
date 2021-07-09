4ba224a018082c10425e5c1cfd78581bfe45a476
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 13:41:02 +0530

    Update : App.js
    - used Animation library
    
    - initalized aos with some custom settings : changed offset and duration of animation

--- pangea_ui/src/App.js
+++ pangea_ui/src/App.js
@@ -1,9 +1,21 @@
+import { useEffect } from "react";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Nav from "./Components/Nav";
-
 import Service from "./Components/Services/Service";
+
+//animation
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+	useEffect(() => {
+		AOS.init({
+			offset: 300,
+			duration: 900,
+		});
+	}, []);
+
 	return (
 		<div className="App">
 			<aside>
