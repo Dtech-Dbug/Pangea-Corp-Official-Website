dfcf3105887f2ea4a73ffd050d3f277ba74c0fdd
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-09 17:20:49 +0530

    docs : create contact.js
    - imported contactInfo component here
    
    - added custom className for contact

---
+++ pangea_ui/src/Components/Contact/Contact.js
@@ -0,0 +1,15 @@
+// Import all other contacts here
+// add a nav on top, to navigate etween the diffrent contact region to make the design cleaner
+
+import React from "react";
+import ContactInfo from "./ContactInfo";
+
+const Contact = () => {
+	return (
+		<div style={{ color: "white" }} className="contact-container">
+			<ContactInfo />
+		</div>
+	);
+};
+
+export default Contact;
