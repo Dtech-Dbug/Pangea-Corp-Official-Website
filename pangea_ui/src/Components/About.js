687caf7a2fcaca10f121d3149e2fd66167330af2
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-10 12:22:47 +0530

    update : about.js => corrected same page navigation offset

--- pangea_ui/src/Components/About.js
+++ pangea_ui/src/Components/About.js
@@ -2,34 +2,36 @@ import React from "react";
 
 const About = () => {
 	return (
-		<div data-aos="fade-up" className="container" id="about">
-			<h2>Biography</h2>
+		<div id="about">
+			<div data-aos="fade-up" className="container">
+				<h2>Biography</h2>
 
-			<p>
-				Pangea Corp is a development company that is cutting-edge. Founded by
-				industry experts with the aim of assisting businesses, startups,
-				entrepreneurs, and visionaories in the development of great products for
-				mobile and web platforms.Believing and nurtuturing,innovation,
-				advancement, and development, introducing technology, and much more are
-				all things we do on a daily basis to ensure that each product is
-				awesome.
-			</p>
+				<p>
+					Pangea Corp is a development company that is cutting-edge. Founded by
+					industry experts with the aim of assisting businesses, startups,
+					entrepreneurs, and visionaories in the development of great products
+					for mobile and web platforms.Believing and nurtuturing,innovation,
+					advancement, and development, introducing technology, and much more
+					are all things we do on a daily basis to ensure that each product is
+					awesome.
+				</p>
 
-			<p>
-				We're a dedicated group of qualified professionals who create custom
-				digital solutions. We provide product advice, technical strategy
-				execution, and ongoing support for enterprise and supported start-ups as
-				a full service company.
-			</p>
+				<p>
+					We're a dedicated group of qualified professionals who create custom
+					digital solutions. We provide product advice, technical strategy
+					execution, and ongoing support for enterprise and supported start-ups
+					as a full service company.
+				</p>
 
-			<p>
-				By concentrating on our clients performance and having the customer
-				experience at the forefront of everything we do, we consistently achieve
-				superior results.We are motivatedby the belief that robust technology
-				used in a solution oriented way improves people's lives in meaningful
-				ways.It is our intention that each of our projects contribute to the
-				advancement of this cause.
-			</p>
+				<p>
+					By concentrating on our clients performance and having the customer
+					experience at the forefront of everything we do, we consistently
+					achieve superior results.We are motivatedby the belief that robust
+					technology used in a solution oriented way improves people's lives in
+					meaningful ways.It is our intention that each of our projects
+					contribute to the advancement of this cause.
+				</p>
+			</div>
 		</div>
 	);
 };
