c4993a6d7cb7ce35c5d7916fcde2cee54e1c3316
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-10 12:23:24 +0530

    Update : Contact.js
    - updated animation of child components
    
    - updated the same page navigation offset

--- pangea_ui/src/Components/Contact/Contact.js
+++ pangea_ui/src/Components/Contact/Contact.js
@@ -17,31 +17,37 @@ import AOS from "aos";
 
 const Contact = () => {
 	return (
-		<div className="contact-container" id="contact" data-aos="fade-in">
-			<h2>Contact</h2>
+		<div id="contact">
+			<div className="contact-container" data-aos="fade-in">
+				<h2>Contact</h2>
 
-			<div className="address-container">
-				<ContactInfo
-					data="fade-in"
-					address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
-					contact="+0124 415 8000"
-					email="hello@parngeacrop.com"
-					city="Hyderabad"
-				/>
-				<ContactInfo
-					data="slide-right"
-					address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
-					contact="+0124 415 8000"
-					email="hello@parngeacrop.com"
-					city="West Lafayette"
-				/>
-				<ContactInfo
-					data="test"
-					address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
-					contact="+0124 415 8000"
-					email="hello@parngeacrop.com"
-					city="Baguio"
-				/>
+				<div className="address-container">
+					<ContactInfo
+						data="slide-down"
+						address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
+						contact="+0124 415 8000"
+						email="hello@parngeacrop.com"
+						city="Hyderabad"
+						country="India"
+					/>
+					<ContactInfo
+						data="slide-right"
+						address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
+						contact="+0124 415 8000"
+						email="hello@parngeacrop.com"
+						city="West Lafayette"
+						country="USA"
+					/>
+					<ContactInfo
+						data="slide-up"
+						offset="200"
+						address="82/A, Usha Jubilee Town 36, 4th Floor, 8-2-293, Rd Number 36, Jubilee Hills, Hyderabad, Telangana 500033"
+						contact="+0124 415 8000"
+						email="hello@parngeacrop.com"
+						city="Baguio"
+						country="Phillipines"
+					/>
+				</div>
 			</div>
 		</div>
 	);
