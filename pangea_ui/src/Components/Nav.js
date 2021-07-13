8f6ea593e2ea53d5d570ce8e72fcf1b8526d79fb
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-13 13:24:56 +0530

    update : Nav.js => added class to nav lists matching the section in App for navigation animation

--- pangea_ui/src/Components/Nav.js
+++ pangea_ui/src/Components/Nav.js
@@ -3,19 +3,19 @@ const Nav = () => {
 	return (
 		<nav id="nav" class="nav">
 			<ul class="nav-container">
-				<li id="home" class="nav-item home">
+				<li id="home" class="nav-item home-section active">
 					<a href="#home-sec">Home</a>
 				</li>
 
-				<li id="about" class="nav-item about">
+				<li id="about" class="nav-item about-section">
 					<a href="#about-sec">About</a>
 				</li>
 
-				<li id="service" class="nav-item service">
+				<li id="service" class="nav-item service-section">
 					<a href="#services-sec">Services</a>
 				</li>
 
-				<li id="contact" class="nav-item contact">
+				<li id="contact" class="nav-item contact-section">
 					<a href="#contact-sec">Contact Us</a>
 				</li>
 			</ul>
