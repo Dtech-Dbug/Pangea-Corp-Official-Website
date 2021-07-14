afa01ab19cebf563e1be448620c137918b710b29
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-14 18:35:01 +0530

    crate : workWithUs.js => component with single text

---
+++ pangea_ui/src/Components/Services/WorkWithUS.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WorkWithUS = () => {
+	return (
+		<div data-aos="fade-up" class="container">
+			<h2>
+				Have a project in mind?
+				<br />
+				Work with us!
+			</h2>
+		</div>
+	);
+};
+
+export default WorkWithUS;
