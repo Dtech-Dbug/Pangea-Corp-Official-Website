453405799b0c0531a503d3ae78467221467e96a4
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-07-19 23:12:28 +0530

    update : Home.js => Added logo for light mode

--- pangea_ui/src/Components/Home.js
+++ pangea_ui/src/Components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import LogoLight from "../Images/Pangaeacroplogo(Light).svg";
+import logoDArk from "../Images/Pangaeacroplogo(Dark).svg";
+import DarkLogo from "../Images/Light.svg";
 
 function Home() {
 	return (
 		<div data-aos="fade-down" className="home" id="home-sec">
-			<img src={LogoLight} alt="" />
+			<img src={DarkLogo} alt="" />
 		</div>
 	);
 }
