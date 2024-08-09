00e751304bce39c40738b620eb01f9d8c03a4f16
DC <dc@knownuggets.com>
2024-08-10 00:44:07 +0530

    edit: content

--- pangea_ui/src/Components/Services/AppDev.js
+++ pangea_ui/src/Components/Services/AppDev.js
@@ -2,28 +2,24 @@ import React from "react";
 
 const AppDev = () => {
 	return (
-		<div data-aos="fade-up" className="container">
-			<h2>App Development.</h2>
+    <div data-aos="fade-up" className="container">
+      <h2>Creative Marketing Consultancy.</h2>
 
-			<p>
-				Bring your iOS or Android app to market quickly with a team of
-				experienced mobile app developers and designers. Our software developers
-				and designers provide deep expertise in the top development frameworks,
-				including React Native.{" "}
-			</p>
+      <p>
+        Our team of creative strategists works closely with you to develop
+        customized marketing plans that not only meet your business goals but
+        also inspire and innovate. We bring fresh ideas and bold thinking to
+        help your brand stand out in a crowded marketplace.
+      </p>
 
-			<ul>
-				<li>Fully responsive design and development.</li>
-				<li>React Native mobile app development.</li>
-				<li>Modern frameworks – React.js, Ruby on Rails, Laravel PHP.</li>
-				<li>Python, Vue.js.</li>
-				<li>Full stack development.</li>
-				<li>Google Cloud + AWS infrastructure planning + Management.</li>
-				<li>AI & machine learning.</li>
-				<li>Internet of Things (IoT). </li>
-			</ul>
-		</div>
-	);
+      <ul>
+        <li>We prioritize marketing campaigns that deliver measurable results and drive business growth.</li>
+        <li>Our focus is on achieving tangible outcomes that contribute to your overall business success.</li>
+        <li>Our goal is to inspire innovation within your marketing efforts, ensuring that your brand remains ahead of the curve.</li>
+        <li>We continually seek out new opportunities and trends to keep your marketing fresh and dynamic.</li>
+      </ul>
+    </div>
+  );
 };
 
 export default AppDev;
